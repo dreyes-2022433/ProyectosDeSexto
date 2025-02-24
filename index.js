233a68f5db77f1a09c27513d9e29c2f2b50e217d
Informatica <informatica@alumno.local>
2025-02-24 11:45:16 -0600

    Se ejecuta la función para crear al admin

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import { initServer } from "./configs/app.js"
 import { config } from "dotenv"
 import { connect } from "./configs/mongo.js"
-
+import { createAdmin } from "./src/users/users.controller.js"
+import { defaultCategory } from "./src/category/category.controller.js"
 config()
 initServer()
 connect()
+createAdmin()
+defaultCategory()

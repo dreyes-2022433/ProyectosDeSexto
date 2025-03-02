23000fa291e911c372b0dc093dfce4d831235213
Diego Reyes <dreyes-2022433@kinal.edu.gt>
2025-03-01 23:37:44 -0600

    Se agregan funcionalidades claves

--- src/products/products.controller.js
+++ src/products/products.controller.js
@@ -1,10 +1,10 @@
 import Products from './products.model.js'
-
+import Category from '../category/category.model.js'
 
 export const addProducts = async(req,res)=>{
     try{
         let data = req.body
-        let product = await new Products(data)
+        let product =  new Products({...data})
         await product.save()
         return res.send({message: 'The product has been added', product})
     }catch(err){
@@ -17,15 +17,16 @@ export const addProducts = async(req,res)=>{
 export const getAllProducts = async(req, res)=>{
     try{
         const{skip= 0, limit = 20} = req.query
-        let products = await Products.find().populate('category','name -_id')
+        let products = await Products.find()
         .skip(skip)
         .limit(limit)
        if(products.length === 0)return res.status(404).send({message:'Not Products yet'})
+        console.log(products)
         return res.send({
             message:'Products found',
             products
         })
-
+        
     }catch(err){
         console.error(err)
         return res.status(500).send({messag:'General error '})
@@ -38,6 +39,7 @@ export const getAllProducts = async(req, res)=>{
 export const verifyStock = async(req,res)=>{
     try{
         let stock = await Products.find({stock: 0})
+        if(stock.length == 0) return res.send({message:'All products have at leats 1 unit'})
         return res.send({message: 'Products out of stock:', stock})
     }catch(err){
         console.error(err)
@@ -61,6 +63,42 @@ export const updateProducto = async (req,res)=>{
         })
     }
 
+}
+
+export const mostselledProducts = async(req,res)=>{
+    try{
+        let mostselledProducts = await Products.find({})
+        .sort({updateCount: -1}).exec()
+        return res.send({message: 'The most selled Products are:',mostselledProducts})
+    }catch(err){
+        console.error(err)
+        return res.send({
+            message: 'general error',err
+        })
+    }
+}
+
+export const findProduct = async(req,res)=>{
+    try{
+        let {name, category} = req.body
+
+        let product = await Products.find({
+            $or:
+            [
+                {name: name},
+                {category: category}
+            ]
+        })
+
+        if(!product) return res.send({message: 'Not product found with this param'})
+            
+        return res.send({message: 'product found',product})
+    }catch(err){
+        console.error(err)
+        return res.send({
+            message: 'general error',err
+        })
+    }
 }
 
 export const deleteProduct = async(req,res)=>{

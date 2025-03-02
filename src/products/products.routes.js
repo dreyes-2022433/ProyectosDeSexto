a328c65c7d663c9eaef9f24d4aed6991783383f1
Diego Reyes <dreyes-2022433@kinal.edu.gt>
2025-03-01 23:39:22 -0600

    Se agregan las rutas de productos

--- src/products/products.routes.js
+++ src/products/products.routes.js
@@ -3,7 +3,10 @@ import {
     addProducts,
     deleteProduct,
     getAllProducts,
-    updateProducto
+    updateProducto,
+    verifyStock,
+    findProduct,
+    mostselledProducts
     
 } from './products.controller.js'
 import { validateJwt, verifyAdminRole } from '../../middlewares/validate.jwt.js'
@@ -15,7 +18,13 @@ api.post('/',[validateJwt, verifyAdminRole, productsValidator], addProducts)
 
 api.get('/',[validateJwt],getAllProducts)
 
+api.get('/mostselledProducts',[validateJwt],mostselledProducts)
+
+api.get('/fp',[validateJwt],findProduct)
+
 api.delete('/',[validateJwt,verifyAdminRole] ,deleteProduct)
+
+api.get('/cero',[validateJwt,verifyAdminRole],verifyStock)
 
 api.put('/',[validateJwt,verifyAdminRole,productsUpdateValidator],updateProducto)
 
